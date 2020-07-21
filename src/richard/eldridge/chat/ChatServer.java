1d86cd3b102f36e4b076ba58f7bad91607b3bc0b
Dridge <richard.eldridge2010@gmail.com>
2020-07-21 14:07:03 +0100

    initial commit

---
+++ src/richard/eldridge/chat/ChatServer.java
@@ -0,0 +1,25 @@
+package richard.eldridge.chat;
+
+import javax.swing.JFrame;
+
+public class ChatServer extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public ChatServer() {
+		initGui();
+		setTitle("Chat Server");
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private void initGui() {
+		
+	}
+
+}

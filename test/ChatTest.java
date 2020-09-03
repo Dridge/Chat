4bff7cb236d463a8dbd9ae817f7120ddf7394b37
Dridge <richard.eldridge2010@gmail.com>
2020-09-03 18:34:42 +0100

    ChatTest initial introduction

--- test/ChatTest.java
+++ test/ChatTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+import richard.eldridge.chat.Chat;
+
+import java.awt.*;
+
 public class ChatTest {
+    Chat chat;
+
+    @Before
+    public void setup() {
+        chat = new Chat();
+    }
+
+    @Test
+    public void testLoginAndClose() throws Exception {
+        Robot robot = new Robot();
+        chat.run();
+        robot.mouseMove(10,10);
+        //robot.keyPress(InputEvent.getMaskForButton(0));
+    }
 }
